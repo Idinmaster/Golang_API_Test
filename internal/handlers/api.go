2b6544c27537dfb844db18c2d43847c7224f0e44
LAPTOP-RFDL1QJT\ASUS <idinmaster.1004.2015@gmail.com>
2025-10-17 10:04:01 +0700

    Update API

---
+++ internal/handlers/api.go
@@ -0,0 +1,20 @@
+package handlers
+
+import (
+	"github.com/go-chi/chi"
+	chimiddle "github.com/go-chi/chi/middleware"
+	"github.com/Idinmaster/Golang_API_Test/internal/middleware"
+)
+
+func Handler(r *chi.Mus) {
+	// Global middleware
+	r.Use(chimiddle.StripSlashes)
+
+	r.Route("/account", func(router chi.Router) {
+
+		// Middleware for /account route
+		router.Use(middleware.Authorization)
+
+		router.Get("/coins", GetCoinBalance)
+	})
+}
